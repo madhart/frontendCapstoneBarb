b6ee830e6c21a528c3c552649d1677d6f8751a4d
Eric Stock <eric.stock@K202Domain.CSN.com>
2026-03-12 14:46:18 -0230

    Setup nextjs project to use sanity client libraries and code

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
 import Image from "next/image";
+import {client} from "@/sanity/lib/client.js"
+import { defineQuery } from "next-sanity";
+import { PortableText } from "@portabletext/react";
 
-export default function Home() {
+
+export default async function Home() {
+  const GREETING_QUERY = defineQuery("*[_type=='greetingItem']{content}")
+
+  const greeting = await client.fetch(GREETING_QUERY)
+
   return (
     <>
       <h2>Rick and Morty Page</h2>
       <p>Welcome to the page, checkout some of the characters</p>
       <Image alt="Eric" src="/Eric.jpg" width={200} height={200}></Image>
+      {greeting.map((item, index) => (
+        <PortableText key={index} value={item.content} />
+      ))}
     </>
   );
 }
