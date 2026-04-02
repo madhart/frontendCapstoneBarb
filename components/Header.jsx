418c25f168b79886447634f118f4cf1dc89669eb
Alisha5555 <alisha.dixon15@ed.cna.nl.ca>
2026-04-02 10:29:46 -0230

    Events page added, contact component added, Header edited.

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,9 +7,11 @@ export default function Header() {
         {/* Header component with logo and navigation links, temporarily the image is out of position and is a placeholder. Styling will be added later */}
         <Image alt="Bicycle NL Logo" src="/next.svg" width={100} height={100}></Image>
         <Link href="/">Home</Link>
-        <Link href="/characters">Characters</Link>
+        
         <Link href="/company">Company</Link>
             <Link href="/contact">Contact</Link>
+            <Link href="/events">Events</Link>
+            <Link href="/membership">Membership</Link>
         </>
     )
 }
